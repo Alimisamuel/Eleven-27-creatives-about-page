efc706c21ba7d9950ec72ba9d317bfaffd608429
Alimisamuel <Alimisamuel121@gmail.com>
2022-08-16 12:15:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# ELEVEN27 CREATIVES ABOUT US PAGE
+
+## Description
+
+Setup Procedure
+
+
+download Raleway font
+## Project setup
+
+```
+open folder on vscode and Go Live
+```
+
+
+
+
+
+![Alimisamuel-portfolio!](./Screenshot%202022-08-16%20at%2012.11.57.png)
---
+++ triangle.js
@@ -0,0 +1,28 @@
+let input = "a"
+for (let i = 1; i < 5; i++) {
+if (i===1){
+
+console.log(input)
+
+continue;
+}
+// let result = triangle(i);
+// for (j = result; j > 0; j--) {
+//     input += input
+//     console.log(input)
+
+// }
+
+if (i===2){
+
+    console.log()
+    
+}
+}
+
+
+
+function triangle(i){
+
+    return (2 * i) - 1
+}
